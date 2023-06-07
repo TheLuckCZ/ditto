eae967cd87d8ba5cb57f30ad51da28b901ce9ced
Tomas Stesti <stesti.tm@gmail.com>
2023-06-07 09:29:59 +0200

    Initial commit

---
+++ src/test/java/cz/luck/ditto/DittoApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.luck.ditto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DittoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
